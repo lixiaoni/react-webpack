727186cc751ef99371b4c225e84d911be10b8389
biubiu <yb634246733@163.com>
2018-05-26 18:06:01 +0800

    init

---
+++ app/components/Tab1.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var Tab1= React.createClass({
+    render: function () {
+        return <div>React+Webpack+ES6从环境搭建到HelloWorld </div>;
+    }
+});
+
+module.exports = Tab1;
