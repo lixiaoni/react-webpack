057849fc3ee138570015657e6284c9534c53b719
biubiu <634246733@qq.com>
2018-05-30 22:46:12 +0800

    Update index.js
    增加mock判断条件

--- 05/src/index.js
+++ 05/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './views/Index';
-import Mock from './mockajax/index';
+//import Mock from './mockajax/index';
+process.env.MOCK && require('@/mockajax')
+
 
 ReactDOM.render(<Index/>, document.getElementById('app'));
